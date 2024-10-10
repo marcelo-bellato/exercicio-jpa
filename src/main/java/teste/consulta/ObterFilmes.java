52c19903efb385763719bee73f82c53f1ee59afd
MarceloBellato <bellato.consultor@gmail.com>
2024-10-09 21:19:56 -0300

    infra

---
+++ src/main/java/teste/consulta/ObterFilmes.java
@@ -0,0 +1,27 @@
+package teste.consulta;
+
+
+import infra.DAO;
+import modelo.muitosparamuitos.Ator;
+import modelo.muitosparamuitos.Filme;
+
+import java.util.List;
+
+public class ObterFilmes {
+
+	public static void main(String[] args) {
+		
+		DAO<Filme> dao = new DAO<>(Filme.class);
+		List<Filme> filmes = dao.consultar(
+				"obterFilmesComNotaMaiorQue", "nota", 8.5);
+		
+		for(Filme filme: filmes) {
+			System.out.println(filme.getNome() 
+					+ " => " + filme.getNota());
+			
+			for(Ator ator: filme.getAtores()) {
+				System.out.println(ator.getNome());
+			}
+		}
+	}
+}
